19d9f4abf7799c61d1549cfc8770745d93ccbf07
Emin Albayrak <56449867+eminalbayrak67@users.noreply.github.com>
2019-10-14 12:32:06 +0300

    seflink array element "." change to "-"

--- ifo.php
+++ ifo.php
@@ -68,8 +68,8 @@ class ifo extends pdo
 	
 		public static function seflink($string)
 		{/* Returns search engine friendly link */
-			$find = array('Ç', 'Ş', 'Ğ', 'Ü', 'İ', 'Ö', 'ç', 'ş', 'ğ', 'ü', 'ö', 'ı', '+', '#');
-			$replace = array('c', 's', 'g', 'u', 'i', 'o', 'c', 's', 'g', 'u', 'o', 'i', 'plus', 'sharp');
+			$find = array('Ç', 'Ş', 'Ğ', 'Ü', 'İ', 'Ö', 'ç', 'ş', 'ğ', 'ü', 'ö', 'ı', '+', '#','.');
+			$replace = array('c', 's', 'g', 'u', 'i', 'o', 'c', 's', 'g', 'u', 'o', 'i', 'plus', 'sharp','-');
 			$string = strtolower(str_replace($find, $replace, $string));
 			$string = preg_replace("@[^A-Za-z0-9\-_\.\+]@i", ' ', $string);
 			$string = trim(preg_replace('/\s+/', ' ', $string));
